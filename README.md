e19cb41aa201e40900348008852f9c7672d034ce
Rodrigo Nicacio <rodrigonicaci@gmail.com>
2023-06-30 01:25:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sistema Bancario
+
+Sistema de Banco Modelo v1
+
+Permitindo o usuario a acessar:
+
+- deposito
+- saque
+- extrato
+
+Desafio de Projeto da DIO
---
+++ sistemaBancario.py
@@ -0,0 +1,70 @@
+menu = """
+[d] para Depositar 
+[s] para Sacar 
+[e] para ver o Extrato
+[n] para Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ''
+saque_limite = 3
+sistema = True
+
+while sistema == True: 
+  print(f"\nSaldo Atual: R${format(saldo,'.2f')}\n")
+  opcao = input(menu)
+
+  if opcao == "d":
+    # extrato
+    deposito = float(input("digite quanto deseja depositar: "))
+
+    if deposito < 0:
+      print("\n\n valor invalido")
+      retorno = input("voltar para o menu ? [s][n]")
+      sistema = True if retorno == 's' else False
+    else:
+      saldo+= deposito
+      extrato += (f"Deposito de: R${format(deposito,'.2f')} \n") 
+
+  elif opcao == "s": 
+    #saque 
+    if saque_limite > 0:
+      print(f"o seu saldo atual é de R${format(saldo,'.2f')} \n\n")
+      saque = float(input("quanto você deseja sacar: "))
+      if saque <= 500.00:
+        if saldo > saque:
+          extrato += (f"O valor sacado foi: R${format(saque,'.2f')} \n") 
+          saque_limite-= 1
+          saldo-= saque
+        else:
+          print("\n\n--------Saldo Insuficiente------------")
+          retorno = input("voltar para o menu ? [s][n]")
+          sistema = True if retorno == 's' else False
+      else:
+        print("\n\n--------Saque acima do limite permitido------------")
+        retorno = input("\nvoltar para o menu ? [s][n]")
+        sistema = True if retorno == 's' else False
+    else:
+      print("\n----------limite de saque diario atingido----------\n")
+      retorno = input("voltar para o menu ? [s][n]")
+      sistema = True if retorno == 's' else False
+  elif opcao == "e":
+    if extrato == '':
+      print("\n----------Extrato----------\n")
+      print("Não foram realizados movimentos")
+      print("\n---------------------------\n")
+
+    else:
+      print("\n----------Extrato----------\n")
+      print(extrato)
+      print(f"Saldo total: R${format(saldo,'.2f')}")
+      print("\n---------------------------\n")
+
+  elif opcao == "n":
+    sistema = False
+  else:
+    print("\n------------comando invalido------------")
+
+
